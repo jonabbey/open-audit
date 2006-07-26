48eded576c2e20636857e874b99ed76654f85edb
aconbere <aconbere@0fda6dae-eacc-4bf0-a90c-2bab3023e2ff>
2006-07-26 06:18:51 +0000

    I've updated this to have the content surrounded by a tab and to have "search" be a label for the search field.  This is not only xhtml compliant but also more symantic.
    git-svn-id: svn://svn.code.sf.net/p/open-audit/code/trunk@18 0fda6dae-eacc-4bf0-a90c-2bab3023e2ff

--- include_right_column.php
+++ include_right_column.php
@@ -1,20 +1,22 @@
 <?php
 
-echo "<td width=\"170\" valign=\"top\" align=\"center\">\n";
-echo "<div class=\"main_each\">\n";
-
-echo $l_ver . " " . $version . "<br /><br />\n";
-echo "Mark Unwin, 2006.<br /><br />\n";
-echo "<a href=\"http://www.open-audit.org\">Open-AudIT</a> $l_wea.<br /><br />\n";
-
-echo "<form action=\"search.php\" method=\"post\">\n";
-echo $l_sea . "<br />\n";
-echo "<input size=\"15\" name=\"search_field\" />\n";
-echo "<input name=\"submit\" value=\"Go\" type=\"submit\" />\n";
-echo "</form>\n";
-
-echo "</div>\n";
-echo "</td></tr>\n";
-echo "</table>\n";
+echo 
+   "<td width=\"170\" valign=\"top\" align=\"center\">
+      <div class=\"main_each\">
+        $l_ver $version<br /><br />
+        Mark Unwin, 2006.<br /><br />
+        <a href=\"http://www.open-audit.org\">Open-AudIT</a> $l_wea.<br /><br />
+  
+        <form action=\"search.php\" method=\"post\">
+          <div>
+            <label for=\"search_field\">$l_sea</label>
+            <input size=\"15\" name=\"search_field\" id=\"search_field\"/>
+            <input name=\"submit\" value=\"Go\" type=\"submit\" />
+          </div>
+        </form>
+      </div>
+    </td>
+  </tr>
+</table>\n";
 
 ?>
