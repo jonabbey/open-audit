532e8054da7625e2695e497696c512090ae5831c
andyhull <andyhull@0fda6dae-eacc-4bf0-a90c-2bab3023e2ff>
2006-08-03 10:41:51 +0000

    Update to setup_2.php (last update never made it to CVS. Fixed Password error and version data not posted.
    git-svn-id: svn://svn.code.sf.net/p/open-audit/code/trunk@113 0fda6dae-eacc-4bf0-a90c-2bab3023e2ff

--- setup_2.php
+++ setup_2.php
@@ -11,13 +11,20 @@ echo "  <tr><td class=\"contenthead\">$l_s18.</td></tr>";
 echo "  <tr><td colspan=\"3\"><hr /></td></tr>";
 echo "  <tr><td>$l_s13 " . $mysql_server . " as " . $mysql_user . ".</td>\n";
 
-$sql = "SET PASSWORD FOR '" . $mysql_user . "'@'%' = OLD_PASSWORD('" . $mysql_password . "')";
-$result = mysql_query($sql) or die("<tr><td>$l_s20:<br /><font color=\"red\">" . $sql . "</font></td></tr>\n");
-
+//$sql = "SET PASSWORD FOR '" . $mysql_user . "'@'%' = OLD_PASSWORD('" . $mysql_password . "')";
+//$result = mysql_query($sql) or die("<tr><td>$l_s20:<br /><font color=\"red\">" . $sql . "</font></td></tr>\n");
+//
 mysql_connect($mysql_server, $mysql_user, $mysql_password) or die("<td>Could not connect.</td><td><img src=\"images/button_fail.png\" width=\"16\" height=\"16\" /></td></tr>\n");
+//
 echo "      <td>$l_cop.</td><td><img src=\"images/button_success.png\" width=\"16\" height=\"16\" /></td></tr>\n";
 echo "  <tr><td>Setting up data in tables.</td>";
 mysql_select_db($mysql_database) or die("<td>Could not select database.</td><td><img src=\"images/button_fail.png\" width=\"16\" height=\"16\" /></td></tr>\n");
+//
+$sql = "SET PASSWORD FOR '" . $mysql_user . "'@'%' = OLD_PASSWORD('" . $mysql_password . "')";
+$result = mysql_query($sql) or die("<tr><td>$l_s20:<br /><font color=\"red\">" . $sql . "</font></td></tr>\n");
+//
+//
+//
 $sql = "INSERT INTO config (config_name, config_value) VALUES ('version','06.07.108')";
 $result = mysql_query($sql) or die("<tr><td>$l_s20:<br /><font color=\"red\">" . $sql . "</font></td></tr>\n");
 //echo "<tr><td>" . $sql . "</td></tr>\n";
@@ -32,4 +39,4 @@ include "include_right_column.php";
 include "include_png_replace.php";
 echo "</body>\n";
 echo "</html>\n";
-?>
+?> 
