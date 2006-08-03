8b007bc74da276da2693266c154c11a76cad016b
andyhull <andyhull@0fda6dae-eacc-4bf0-a90c-2bab3023e2ff>
2006-08-03 10:08:30 +0000

    Updated setup_2.php Should finally work as expected.
    git-svn-id: svn://svn.code.sf.net/p/open-audit/code/trunk@112 0fda6dae-eacc-4bf0-a90c-2bab3023e2ff

--- setup_2.php
+++ setup_2.php
@@ -10,6 +10,10 @@ echo "<table border=\"0\" cellpadding=\"2\" cellspacing=\"0\" width=\"100%\" >\n
 echo "  <tr><td class=\"contenthead\">$l_s18.</td></tr>";
 echo "  <tr><td colspan=\"3\"><hr /></td></tr>";
 echo "  <tr><td>$l_s13 " . $mysql_server . " as " . $mysql_user . ".</td>\n";
+
+$sql = "SET PASSWORD FOR '" . $mysql_user . "'@'%' = OLD_PASSWORD('" . $mysql_password . "')";
+$result = mysql_query($sql) or die("<tr><td>$l_s20:<br /><font color=\"red\">" . $sql . "</font></td></tr>\n");
+
 mysql_connect($mysql_server, $mysql_user, $mysql_password) or die("<td>Could not connect.</td><td><img src=\"images/button_fail.png\" width=\"16\" height=\"16\" /></td></tr>\n");
 echo "      <td>$l_cop.</td><td><img src=\"images/button_success.png\" width=\"16\" height=\"16\" /></td></tr>\n";
 echo "  <tr><td>Setting up data in tables.</td>";
@@ -17,8 +21,7 @@ mysql_select_db($mysql_database) or die("<td>Could not select database.</td><td>
 $sql = "INSERT INTO config (config_name, config_value) VALUES ('version','06.07.108')";
 $result = mysql_query($sql) or die("<tr><td>$l_s20:<br /><font color=\"red\">" . $sql . "</font></td></tr>\n");
 //echo "<tr><td>" . $sql . "</td></tr>\n";
-$sql = "SET PASSWORD FOR '" . $mysql_user . "'@'%' = OLD_PASSWORD('" . $mysql_password . "')";
-$result = mysql_query($sql) or die("<tr><td>$l_s20:<br /><font color=\"red\">" . $sql . "</font></td></tr>\n");
+
 //echo "<tr><td>" . $sql . "</td></tr>\n";
 echo "<td>Done.</td><td><img src=\"images/button_success.png\" width=\"16\" height=\"16\" /></td></tr>\n";
 echo "<tr><td>$l_s19. <br />&nbsp;</td></tr>";
