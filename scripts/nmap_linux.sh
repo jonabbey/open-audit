4c874ce6092af92ee78fa8b61388c0ab86686cc4
munwin <munwin@0fda6dae-eacc-4bf0-a90c-2bab3023e2ff>
2006-08-03 22:37:21 +0000

    Added looping for subnets. Not finished, but working.
    git-svn-id: svn://svn.code.sf.net/p/open-audit/code/trunk@121 0fda6dae-eacc-4bf0-a90c-2bab3023e2ff

--- scripts/nmap_linux.sh
+++ scripts/nmap_linux.sh
@@ -1,9 +1,13 @@
 #!/bin/bash
 
-nmap_file="nmap_file.txt"
-nmap_input=`nmap -v -O -oN $nmap_file 192.168.10.101`
-nmap_output=`cat $nmap_file`
-wget --post-data="submit=submit&add=$nmap_output" http://192.168.10.28/oa/admin_nmap_input.php
-#rm "$nmap_file"
-#rm "admin_nmap_input.php"
 
+for ((a=38; a <= 255 ; a++))
+do
+echo "192.168.10.$a"
+nmap_file="nmap_file.txt"
+nmap_input=`nmap -v -O -oN $nmap_file 192.168.10.$a`
+nmap_output=`cat $nmap_file`
+wget -q --post-data="submit=submit&add=$nmap_output" http://192.168.10.28/oa/admin_nmap_input.php
+rm "$nmap_file"
+rm "admin_nmap_input.php"
+done
