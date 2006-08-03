b17a4438ebcc3eb6a20ccaf6f72c04de8da18d13
munwin <munwin@0fda6dae-eacc-4bf0-a90c-2bab3023e2ff>
2006-08-03 01:47:08 +0000

    Removed Firewall & AntiVirus sections when a PC is not running Windows
    git-svn-id: svn://svn.code.sf.net/p/open-audit/code/trunk@111 0fda6dae-eacc-4bf0-a90c-2bab3023e2ff

--- system_security.php
+++ system_security.php
@@ -15,95 +15,92 @@ $opt_count = 0;
 $SQL = "SELECT * FROM system WHERE system_uuid = '$pc' AND system_timestamp = '$timestamp'";
 $result = mysql_query($SQL, $db);
 $myrow = mysql_fetch_array($result);
-echo "<tr><td colspan=\"4\" class=\"contenthead\"><img src=\"images/firewall_l.png\" width=\"48\" height=\"48\" alt=\"\" /> $l_xpf</td></tr>";
-echo "<tr><td colspan=\"4\"><b>General Settings - Domain.</b></td></tr>";
-
-if (($myrow["firewall_enabled_domain"] == "1") OR ($myrow["firewall_enabled_domain"] == "0")) {
-  $enabled = $myrow["firewall_enabled_domain"];
-  if ($enabled == "0") { $enabled = "No";} else { }
-  if ($enabled == "1") { $enabled = "Yes";} else { }
-  $notifications = $myrow["firewall_disablenotifications_domain"];
-  if ($notifications == "0") { $notifications = "No"; } else {}
-  if ($notifications == "1") { $notifications = "Yes"; } else {}
-  $excep = $myrow["firewall_donotallowexceptions_domain"];
-  if ($excep == "0") { $excep = "No"; } else {}
-  if ($excep == "1") { $excep = "Yes"; } else {}
-  echo "<tr bgcolor=\"$bg1\"><td width=\"25%\">$l_fiw:&nbsp;</td><td width=\"25%\">" . $enabled . "</td><td width=\"25%\"></td><td width=\"25%\"></td></tr>\n";
-  echo "<tr><td>$l_not:&nbsp;</td><td>" . $notifications . "</td><td></td><td></td></tr>\n";
-  echo "<tr bgcolor=\"$bg1\"><td>$l_exp:&nbsp;</td><td>" . $excep . "</td><td></td><td></td></tr>\n";
-} else {
-  echo "<tr bgcolor=\"$bg1\"><td colspan=\"2\">$l_doo.</td><td></td><td></td></tr>\n";
-}
-
-echo "<tr><td colspan=\"4\"><b>$l_ges.</b></td></tr>";
-$bgcolor = "#FFFFFF";
-if (($myrow["firewall_enabled_standard"] == "1") OR ($myrow["firewall_enabled_standard"] == "0")) {
-  $enabled = $myrow["firewall_enabled_standard"];
-  if ($enabled == "0") { $enabled = "No";} else { }
-  if ($enabled == "1") { $enabled = "Yes";} else { }
-  $notifications = $myrow["firewall_disablenotifications_standard"];
-  if ($notifications == "0") { $notifications = "No"; } else {}
-  if ($notifications == "1") { $notifications = "Yes"; } else {}
-  $excep = $myrow["firewall_donotallowexceptions_standard"];
-  if ($excep == "0") { $excep = "No"; } else {}
-  if ($excep == "1") { $excep = "Yes"; } else {}
-  echo "<tr bgcolor=\"$bg1\"><td>$l_fiw:&nbsp;</td><td>" . $enabled . "</td><td></td><td></td></tr>\n";
-  echo "<tr><td>$l_not:&nbsp;</td><td>" . $notifications . "</td><td></td><td></td></tr>\n";
-  echo "<tr bgcolor=\"$bg1\"><td>$l_exp:&nbsp;</td><td>" . $excep . "</td><td></td><td></td></tr>\n";
-} else {
-  echo "<tr bgcolor=\"$bg1\"><td colspan=\"2\">$l_gen.</td><td></td><td></td></tr>\n";
-}
-
-$SQL = "SELECT * FROM firewall_ports where port_uuid = '$pc' AND port_timestamp = '$timestamp' ORDER BY port_profile, port_number";
-$result = mysql_query($SQL, $db);
-$myrow = mysql_fetch_array($result);
+if (substr_count($myrow["system_os_name"], "Microsoft Windows XP") > 0) {
+  echo "<tr><td colspan=\"4\" class=\"contenthead\"><img src=\"images/firewall_l.png\" width=\"48\" height=\"48\" alt=\"\" /> $l_xpf</td></tr>";
+  echo "<tr><td colspan=\"4\"><b>General Settings - Domain.</b></td></tr>";
+  if (($myrow["firewall_enabled_domain"] == "1") OR ($myrow["firewall_enabled_domain"] == "0")) {
+    $enabled = $myrow["firewall_enabled_domain"];
+    if ($enabled == "0") { $enabled = "No";} else { }
+    if ($enabled == "1") { $enabled = "Yes";} else { }
+    $notifications = $myrow["firewall_disablenotifications_domain"];
+    if ($notifications == "0") { $notifications = "No"; } else {}
+    if ($notifications == "1") { $notifications = "Yes"; } else {}
+    $excep = $myrow["firewall_donotallowexceptions_domain"];
+    if ($excep == "0") { $excep = "No"; } else {}
+    if ($excep == "1") { $excep = "Yes"; } else {}
+    echo "<tr bgcolor=\"$bg1\"><td width=\"25%\">$l_fiw:&nbsp;</td><td width=\"25%\">" . $enabled . "</td><td width=\"25%\"></td><td width=\"25%\"></td></tr>\n";
+    echo "<tr><td>$l_not:&nbsp;</td><td>" . $notifications . "</td><td></td><td></td></tr>\n";
+    echo "<tr bgcolor=\"$bg1\"><td>$l_exp:&nbsp;</td><td>" . $excep . "</td><td></td><td></td></tr>\n";
+  } else {
+    echo "<tr bgcolor=\"$bg1\"><td colspan=\"2\">$l_doo.</td><td></td><td></td></tr>\n";
+  }
+  echo "<tr><td colspan=\"4\"><b>$l_ges.</b></td></tr>";
+  $bgcolor = "#FFFFFF";
+  if (($myrow["firewall_enabled_standard"] == "1") OR ($myrow["firewall_enabled_standard"] == "0")) {
+    $enabled = $myrow["firewall_enabled_standard"];
+    if ($enabled == "0") { $enabled = "No";} else { }
+    if ($enabled == "1") { $enabled = "Yes";} else { }
+    $notifications = $myrow["firewall_disablenotifications_standard"];
+    if ($notifications == "0") { $notifications = "No"; } else {}
+    if ($notifications == "1") { $notifications = "Yes"; } else {}
+    $excep = $myrow["firewall_donotallowexceptions_standard"];
+    if ($excep == "0") { $excep = "No"; } else {}
+    if ($excep == "1") { $excep = "Yes"; } else {}
+    echo "<tr bgcolor=\"$bg1\"><td>$l_fiw:&nbsp;</td><td>" . $enabled . "</td><td></td><td></td></tr>\n";
+    echo "<tr><td>$l_not:&nbsp;</td><td>" . $notifications . "</td><td></td><td></td></tr>\n";
+    echo "<tr bgcolor=\"$bg1\"><td>$l_exp:&nbsp;</td><td>" . $excep . "</td><td></td><td></td></tr>\n";
+  } else {
+    echo "<tr bgcolor=\"$bg1\"><td colspan=\"2\">$l_gen.</td><td></td><td></td></tr>\n";
+  }
+  $SQL = "SELECT * FROM firewall_ports where port_uuid = '$pc' AND port_timestamp = '$timestamp' ORDER BY port_profile, port_number";
+  $result = mysql_query($SQL, $db);
+  $myrow = mysql_fetch_array($result);
   echo "<tr><td colspan=\"4\"><b>$l_pot.</b></td></tr>\n";
-if ($myrow = mysql_fetch_array($result)){
-  $bgcolor = "#F1F1F1";
-  do {
-    echo "<tr bgcolor=\"$bgcolor\">\n";
-    echo "  <td>" . $myrow["port_profile"] .  "</td><td>"  . $myrow["port_number"] . "</td>\n";
-    echo "  <td>" . $myrow["port_protocol"] .  "</td><td>"  . $myrow["port_scope"] . "</td>\n";
-    echo "</tr>\n";
-    $bgcolor = change_row_color($bgcolor,$bg1,$bg2);
-  } while ($myrow = mysql_fetch_array($result));
-} else {}
+  if ($myrow = mysql_fetch_array($result)){
+    $bgcolor = "#F1F1F1";
+    do {
+      echo "<tr bgcolor=\"$bgcolor\">\n";
+      echo "  <td>" . $myrow["port_profile"] .  "</td><td>"  . $myrow["port_number"] . "</td>\n";
+      echo "  <td>" . $myrow["port_protocol"] .  "</td><td>"  . $myrow["port_scope"] . "</td>\n";
+      echo "</tr>\n";
+      $bgcolor = change_row_color($bgcolor,$bg1,$bg2);
+    } while ($myrow = mysql_fetch_array($result));
+  } else {}
+  $SQL = "SELECT * FROM firewall_auth_app where firewall_app_uuid = '$pc' AND firewall_app_timestamp = '$timestamp'  ORDER BY firewall_app_profile, firewall_app_name";
+  $result = mysql_query($SQL, $db);
+  $myrow = mysql_fetch_array($result);
+  if ($myrow = mysql_fetch_array($result)){
+    echo "<tr><td colspan=\"4\"><b>$l_pos.</b></td></tr>\n";
+    $bgcolor = "#F1F1F1";
+    do {
+      echo "<tr bgcolor=\"$bgcolor\">\n";
+      echo "  <td>" . $myrow["firewall_app_profile"] . "</td><td>" . $myrow["firewall_app_name"] . "</td>\n";
+      echo "  <td colspan=\"2\">" . $myrow["firewall_app_executable"] . "</td>\n";
+      echo "</tr>\n";
+      $bgcolor = change_row_color($bgcolor,$bg1,$bg2);
+    } while ($myrow = mysql_fetch_array($result));
+  } else {}
+  $opt_count = 0;
+  $SQL = "SELECT * from software WHERE software_uuid = '$pc' AND software_timestamp = '$timestamp' AND software_name LIKE '%ZoneAlarm%'";
+  $result = mysql_query($SQL, $db);
+  if ($myrow = mysql_fetch_array($result)){
+    $bgcolor = "#F1F1F1";
+    do {
+      echo "<tr><td class=\"contenthead\" colspan=\"2\"><img src=\"images/firewall_l.png\" width=\"48\" height=\"48\" alt=\"\" /> Other Firewall</td></tr>\n";
+      echo "<tr bgcolor=\"$bgcolor\"><td>Enabled:</td><td colspan=\"3\">" . $myrow["software_name"] . "</td></tr>\n";
+      $bgcolor = change_row_color($bgcolor,$bg1,$bg2);
+    } while ($myrow = mysql_fetch_array($result));
+  } else {}
+  } // End of Firewall
+}
 
-
-$SQL = "SELECT * FROM firewall_auth_app where firewall_app_uuid = '$pc' AND firewall_app_timestamp = '$timestamp'  ORDER BY firewall_app_profile, firewall_app_name";
-$result = mysql_query($SQL, $db);
-$myrow = mysql_fetch_array($result);
-if ($myrow = mysql_fetch_array($result)){
-  echo "<tr><td colspan=\"4\"><b>$l_pos.</b></td></tr>\n";
-  $bgcolor = "#F1F1F1";
-  do {
-    echo "<tr bgcolor=\"$bgcolor\">\n";
-    echo "  <td>" . $myrow["firewall_app_profile"] . "</td><td>" . $myrow["firewall_app_name"] . "</td>\n";
-    echo "  <td colspan=\"2\">" . $myrow["firewall_app_executable"] . "</td>\n";
-    echo "</tr>\n";
-    $bgcolor = change_row_color($bgcolor,$bg1,$bg2);
-  } while ($myrow = mysql_fetch_array($result));
-} else {}
-
-$opt_count = 0;
-$SQL = "SELECT * from software WHERE software_uuid = '$pc' AND software_timestamp = '$timestamp' AND software_name LIKE '%ZoneAlarm%'";
-$result = mysql_query($SQL, $db);
-if ($myrow = mysql_fetch_array($result)){
-  $bgcolor = "#F1F1F1";
-  do {
-    echo "<tr><td class=\"contenthead\" colspan=\"2\"><img src=\"images/firewall_l.png\" width=\"48\" height=\"48\" alt=\"\" /> Other Firewall</td></tr>\n";
-    echo "<tr bgcolor=\"$bgcolor\"><td>Enabled:</td><td colspan=\"3\">" . $myrow["software_name"] . "</td></tr>\n";
-    $bgcolor = change_row_color($bgcolor,$bg1,$bg2);
-  } while ($myrow = mysql_fetch_array($result));
-} else {}
-} // End of Firewall
 
 if (($sub == "vi") or ($sub == "all")){
 $opt_count = 0;
-$SQL = "SELECT * from system WHERE system_uuid = '$pc' AND system_timestamp = '$timestamp' AND (virus_name <> '' OR virus_manufacturer <> '')";
+$SQL = "SELECT * from system WHERE system_uuid = '$pc' AND system_timestamp = '$timestamp' AND (virus_name <> '' OR virus_manufacturer <> '') AND system_os_name LIKE '%Windows%'";
 $result = mysql_query($SQL, $db);
-echo "<tr><td class=\"contenthead\" colspan=\"4\"><br /><img src=\"images/antivirus_l.png\" width=\"48\" height=\"48\" alt=\"\" /> $l_ant</td><td></td></tr>\n";
 if ($myrow = mysql_fetch_array($result)){
+  echo "<tr><td class=\"contenthead\" colspan=\"4\"><br /><img src=\"images/antivirus_l.png\" width=\"48\" height=\"48\" alt=\"\" /> $l_ant</td><td></td></tr>\n";
   $bgcolor = "#F1F1F1";
   do {
     echo "<tr bgcolor=\"$bgcolor\"><td>$l_nam:</td><td colspan=\"3\">" . $myrow["virus_name"] . "</td></tr>\n";
@@ -124,9 +121,7 @@ if ($myrow = mysql_fetch_array($result)){
       echo "<tr bgcolor=\"$bgcolor\"><td>$l_ver:</td><td colspan=\"3\">" . $myrow["software_version"] . "</td></tr>\n";
       $bgcolor = change_row_color($bgcolor,$bg1,$bg2);
     } while ($myrow = mysql_fetch_array($result));
-  } else {
-    echo "<tr><td>$l_anu:</td><td colspan=\"3\">$l_nti.</td></tr>\n";
-  }
+  } else {}
 }
 } // End of AntiVirus
 
