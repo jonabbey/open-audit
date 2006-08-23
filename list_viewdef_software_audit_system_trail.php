7313bc646977e1256352274d6473b8f059c16fb8
matze123 <matze123@0fda6dae-eacc-4bf0-a90c-2bab3023e2ff>
2006-08-23 07:29:30 +0000

    New upload because of problems in svn
    git-svn-id: svn://svn.code.sf.net/p/open-audit/code/trunk@364 0fda6dae-eacc-4bf0-a90c-2bab3023e2ff

---
+++ list_viewdef_software_audit_system_trail.php
@@ -0,0 +1,44 @@
+<?php
+
+$query_array=array("headline"=>__("List Audit-Trail for Software on Host"),
+                   "sql"=>"
+                          SELECT software_name, software_version, software_first_timestamp, system_name, system_uuid, net_ip_address
+                          FROM software, system
+                          WHERE software_name NOT LIKE '%Hotfix%' AND software_name NOT LIKE '%Update%' AND software_uuid = system_uuid AND software_uuid = '".$_REQUEST["pc"]."' AND software_timestamp = system_timestamp
+                          ",
+                   "sort"=>"software_first_timestamp",
+                   "dir"=>"DESC",
+                   "get"=>array("file"=>"list.php",
+                                "title"=>__("Systems installed this Version of this Software"),
+                                "var"=>array("name"=>"%software_name",
+                                             "version"=>"%software_version",
+                                             "view"=>"systems_for_software_version",
+                                             "headline_addition"=>"%software_name",
+                                            ),
+                               ),
+                   "fields"=>array("10"=>array("name"=>"software_name",
+                                               "head"=>__("Software Name"),
+                                               "show"=>"y",
+                                               "link"=>"y",
+                                               "get"=>array("file"=>"list.php",
+                                                            "title"=>__("Systems installed this Software"),
+                                                            "var"=>array("name"=>"%software_name",
+                                                                         "view"=>"systems_for_software",
+                                                                         "headline_addition"=>"%software_name",
+                                                                        ),
+                                                           ),
+                                              ),
+                                   "20"=>array("name"=>"software_version",
+                                               "head"=>__("Version"),
+                                               "show"=>"y",
+                                               "link"=>"n",
+                                              ),
+
+                                   "30"=>array("name"=>"software_first_timestamp",
+                                               "head"=>__("First Detected"),
+                                               "show"=>"y",
+                                               "link"=>"n",
+                                              ),
+                                  ),
+                  );
+?>
