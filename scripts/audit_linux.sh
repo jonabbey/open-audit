4930355d9d70e819de88207c35095ddc39722b6c
munwin <munwin@0fda6dae-eacc-4bf0-a90c-2bab3023e2ff>
2006-08-23 01:50:48 +0000

    Added partition info. Added exit if not run as root.
    git-svn-id: svn://svn.code.sf.net/p/open-audit/code/trunk@362 0fda6dae-eacc-4bf0-a90c-2bab3023e2ff

--- scripts/audit_linux.sh
+++ scripts/audit_linux.sh
@@ -1,5 +1,10 @@
 #!/bin/bash
 
+if [ $(id -u) != "0" ]; then
+  echo "You must use root privs to run this script."
+  exit 0
+fi
+
 HOSTNAME=`hostname`
 DATE="`date +%d/%m/%Y``date +%H:%M:%S`"
 ReportFile=$HOSTNAME.txt
@@ -45,6 +50,7 @@ RAMsize=`expr $RAMsizekb / 1024`
 
 #Number of CPUs
 nbcpu=`cat /proc/cpuinfo | grep "processor" | wc -l`
+
 # System Model
 sys_model=`lshal --long --show /org/freedesktop/Hal/devices/computer | grep smbios.system.product | cut -d" " -f5 | cut -d"'" -f2`
 chassis_type=`lshal --long --show /org/freedesktop/Hal/devices/computer | grep smbios.chassis.type | cut -d" " -f5 | cut -d"'" -f2`
@@ -139,7 +145,6 @@ echo "system03^^^$mount_point^^^$version^^^Linux^^^$distribution ($release)^^^$c
 #         - Organisation
 #         - Language
 #         - Registered User
-#         - System Version
 
 # Processor
 cpu_device_id=`cat /proc/cpuinfo | grep "processor" | cut -d: -f2 | cut -c2-`
@@ -161,12 +166,13 @@ for i in $perif; do
   type=`echo "$pcilist" | grep -w $i -A 4 | grep -w "Class:" | cut -d":" -f2 | cut -f2`
   name=`echo "$pcilist" | grep -w $i -A 4 | grep -v "[[:digit:]]:[[:digit:]]" | grep -w "Device:" | cut -d":" -f2 | cut -f2`
   manufacturer=`echo "$pcilist" | grep -w $i -A 4 | grep -w "Vendor:" | cut -d":" -f2 | cut -f2`
-  device_id=`echo "$pcilist" | grep -w $i -A 4 | grep -w "Device:" | cut -d":" -f2 | cut -f2`
-
+  device_id=`echo $i`
+  
   # Graphic Card
   if [ "$type" = "VGA compatible controller" ]
   then
-    echo "video^^^ ^^^$manufacturer - $name^^^0^^^0^^^0^^^0^^^$manufacturer - $name^^^0000-00-00^^^ ^^^ ^^^ ^^^$device_id^^^" >> $ReportFile
+    sss=`echo "video^^^ ^^^$manufacturer - $name^^^0^^^0^^^0^^^0^^^$manufacturer - $name^^^0000-00-00^^^ ^^^ ^^^ ^^^$device_id^^^"`
+    echo "$sss" >> $ReportFile
   fi
   # Missing - Adapter Ram
   #         - Hor Res
@@ -188,7 +194,7 @@ done
 # Software
 packages="apt azureus bash build-essential cdparanoia cdrdao cdrecord cpp cron cupsys cvs dbus dhcp3-client diff dpkg epiphany-browser esound evolution firefox flashplugin-nonfree foomatic-db g++ gaim gcc gdm gedit gimp gnome-about gnucash gnumeric gtk+ httpd inkscape iptables k3b kdebase koffice libgnome2-0 linux-image-386 metacity mozilla-browser mysql-admin mysql-query-browser mysql-server-4.1 nautilus openoffice.org openssh-client openssh-server perl php4 php5 postfix postgresql python python2.4 rdesktop rhythmbox samba-common sendmail smbclient subversion sun-j2re1.5 swf-player synaptic thunderbird tsclient udev vim vlc vnc-common webmin xfce xmms xserver-xorg"
 for name in $packages; do
-  version=`dpkg --list | grep "$name " |tail -n1|awk '{print $3}' 2> /dev/null`
+  version=`dpkg --list | grep "  $name " |tail -n1|awk '{print $3}' 2> /dev/null`
   if [ "$version" ] 
   then
     echo "software^^^$name^^^$version^^^^^^^^^^^^^^^^^^^^^^^^^^^" >> $ReportFile
@@ -237,12 +243,12 @@ do
         then
           let "size = $size * 1024"
         fi
-        echo "harddrive^^^$mount^^^ ^^^$bus^^^$vendor^^^$product^^^1^^^^^^^^^^^^$size^^^^^^" >> $ReportFile
-        # Missing - Partitions (1)
-        #         - scsi bus
+        echo "harddrive^^^$mount^^^ ^^^$bus^^^$vendor^^^$product^^^$count^^^^^^^^^^^^$size^^^^^^" >> $ReportFile
+        # Missing - scsi bus
         #         - scsi logical unit
         #         - scsi port
         #         - pnp id
+        count=0
       fi
     else
       if [ "$bus" = "usb" ]
@@ -253,13 +259,40 @@ do
   fi
 done
 
+#Partitions
+for j in `df -l -T -x tmpfs |awk '{print $1}'`
+do
+  part_capt=`df -l -T -x tmpfs | grep $j | awk '{print $7}'`
+  if [ "$part_capt" = "/" ]; then
+    part_boot="True"
+  else
+    part_boot="False"
+  fi
+  part_perc=`df -l -T -x tmpfs | grep $j |awk '{print $6}'`
+  part_name=`df -l -T -x tmpfs | grep $j |awk '{print $1}'`
+  part_form=`df -l -T -x tmpfs | grep $j |awk '{print $2}'`
+  part_size=`df -l -T -x tmpfs | grep $j |awk '{print $3}'`
+  part_size=`expr $part_size / 1`
+  part_size=`expr $part_size / 1024`
+  part_aval=`df -l -T -x tmpfs | grep $j |awk '{print $5}'`
+  part_aval=`expr $part_aval / 1`
+  part_aval=`expr $part_aval / 1024`
+  if [ "$part_form" != "Type" ]; then
+    echo "partition^^^$part_boot^^^$part_boot^^^ ^^^ ^^^$part_perc^^^$part_boot^^^$part_name^^^$part_form^^^$part_aval^^^$part_size^^^$part_name^^^" >> $ReportFile
+  fi
+  # Missing - DeviceID
+  #         - Disk Index
+  #         - File System
+done
+
 # Users
 users=`cat /etc/passwd | cut -d":" -f1`
 for i in $users
 do
   username=`cat /etc/passwd | grep $i | cut -d":" -f5 | tr -s ',' | tr ',' ' '`
   user_id=`cat /etc/passwd | grep $i | cut -d":" -f3`
-  echo "l_user^^^^^^^^^$username^^^$i^^^^^^^^^^^^$user_id^^^" >> $ReportFile
+#  echo "l_user^^^^^^^^^$username^^^$i^^^^^^^^^^^^$user_id^^^" >> $ReportFile
+  printf "l_user^^^^^^^^^%s^^^%s^^^^^^^^^^^^%s^^^\n" $username $i $user_id >> $ReportFile
 done
 
 # The end - submit to Open-AudIT
