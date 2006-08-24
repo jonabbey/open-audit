b9b8084c077ca2c5482924562d611ef6180070da
matze123 <matze123@0fda6dae-eacc-4bf0-a90c-2bab3023e2ff>
2006-08-24 22:12:41 +0000

    Move drawing to functions
    git-svn-id: svn://svn.code.sf.net/p/open-audit/code/trunk@403 0fda6dae-eacc-4bf0-a90c-2bab3023e2ff

--- system_print.php
+++ system_print.php
@@ -6,31 +6,22 @@ function microtime_float()
 }
 $time_start = microtime_float();
 
-//Col-Width and Hight
-$col_width_1=40;
-$col_width_2=130;
-$col_height_1=5;
-$font_size_headline_1=14;
-$font_size_headline_2=10;
-$font_size_body=10;
-$font_size_footer=6;
+// If you would like to have a new View, you have to modify 3 parts:
+// -> include_menu_array.php: $menue_array
+// -> system_viewdef_X.php: "Table and fields to select and show"
+// -> option: include_functions.php: special_field_converting()
 
 include_once("include_config.php");
 include_once("include_functions.php");
 include_once("include_win_type.php");
 
-//MySQL-Connect
-$db = mysql_connect($mysql_server,$mysql_user,$mysql_password) or die('Could not connect: ' . mysql_error());
-mysql_select_db($mysql_database,$db);
-
 //Include PDF-Libaries
 define('FPDF_FONTPATH','./lib/fpdf/font/');
 require('./lib/fpdf/fpdf.php');
 
-// If you would like to have a new View, you have to modify 3 parts:
-// -> include_menu_array.php: $menue_array
-// -> system_viewdef_X.php: "Table and fields to select and show"
-// -> option: system.php: "Special field-converting"
+//MySQL-Connect
+$db = mysql_connect($mysql_server,$mysql_user,$mysql_password) or die('Could not connect: ' . mysql_error());
+mysql_select_db($mysql_database,$db);
 
 //Include the view-definition
 if(isset($_REQUEST["view"]) AND $_REQUEST["view"]!=""){
@@ -45,10 +36,9 @@ if(is_file($include_filename)){
     die("FATAL: Could not find view $include_filename");
 }
 
+//Set GET[pc] to a local variable
 if(isset($_GET["pc"]) AND $_GET["pc"]!=""){
     $pc=$_GET["pc"];
-}else{
-    die("FATAL: No pc given");
 }
 
 //Convert GET[category] to an array
@@ -66,24 +56,76 @@ if(isset($array_category) AND is_array($array_category) AND $_REQUEST["category"
     }
 }
 
+//Col-Width and Hight
+$draw["col_width_1"]=40;
+$draw["col_width_2"]=130;
+$draw["col_height_1"]=4;
+$draw["font_size_headline_1"]=14;
+$draw["font_size_headline_2"]=10;
+$draw["font_size_body"]=10;
+$draw["font_size_footer"]=6;
 
-
- //Create PDF
- $pdf=new FPDF();
- $pdf->Open();
- $pdf->AddPage('P');
- $pdf->SetCompression( TRUE );
- //Schriftfarbe
- $pdf->SetTextColor(0,0,0);
- //Ersteller
- $pdf->SetCreator('Open-AudIT');
- //display-Mode
-// $pdf->SetDisplayMode('fullpage');
- //Seitenränder
- $pdf->SetMargins(15,0,0);
- //Seitentitel
- $pdf->SetTitle('Report');
- $pdf->SetAutoPageBreak( TRUE );
+function pdf_start(){
+    //Create PDF
+    $pdf=new FPDF();
+    $pdf->Open();
+    $pdf->SetCompression( TRUE );
+    //Font-Color
+    $pdf->SetTextColor(0,0,0);
+    //Creator
+    $pdf->SetCreator('Open-AudIT');
+    //Display-Mode
+    //$pdf->SetDisplayMode('fullpage');
+    //Title
+    $pdf->SetTitle('Report');
+    //Auto-Page-Break
+    $pdf->SetAutoPageBreak( TRUE );
+    return $pdf;
+}
+function pdf_end($pdf){
+    //Send PDF
+    $pdf->Output();
+    $pdf->Close();
+    return $pdf;
+}
+function pdf_draw_headline_1($pdf, $draw, $show_value){
+     $pdf->SetFont('Arial','B',$draw["font_size_headline_1"]);
+     $pdf->Cell(50,7,$show_value,0,1,'L');
+     return $pdf;
+}
+function pdf_draw_headline_2($pdf, $draw, $show_value){
+    $pdf->SetFont('Arial','B',$draw["font_size_headline_2"]);
+    $pdf->Cell(50,7,$show_value,0,1,'L');
+    return $pdf;
+}
+//Start new Page
+function pdf_draw_new_page($pdf, $draw){
+    $pdf->AddPage('P');
+    $pdf->SetMargins(15,10,0);
+    return $pdf;
+}
+function pdf_draw_footer($pdf, $draw){
+    //Print Footer
+    $pdf->SetFont('Arial','B',$draw["font_size_footer"]);
+    $pdf->SetY(-15);
+    $pdf->Write(5,date("Y-m-d, H:i"),0,0,'L');
+    $pdf->SetX(100);
+    $pdf->Write(5,$pdf->PageNo());
+    $pdf->SetX(170);
+    $pdf->Write(5,'www.open-audit.org','http://www.open-audit.org');
+    return $pdf;
+}
+function pdf_draw_row($pdf, $draw, $show_value_1, $show_value_2){
+    $pdf->Cell($draw["col_width_1"],$draw["col_height_1"],$show_value_1,'B',0,'L');
+    $pdf->Cell($draw["col_width_2"],$draw["col_height_1"],$show_value_2,'B',1,'L');
+    return $pdf;
+}
+function pdf_draw_ln($pdf){
+    $pdf->Ln();
+    return $pdf;
+}
+//Start PDF
+$pdf=pdf_start();
 
 if(1==1){
 
@@ -91,9 +133,10 @@ if(1==1){
          $headline_addition="";
      }
 
+     //Create new Page
+     $pdf=pdf_draw_new_page($pdf, $draw);
      //Headline on the first Page
-     $pdf->SetFont('Arial','B',$font_size_headline_1);
-     $pdf->Cell(50,7,$query_array["name"]." ".$headline_addition,0,1,'L');
+     $pdf=pdf_draw_headline_1($pdf, $draw, $query_array["name"]." ".$headline_addition);
 
     //Show each Category
     reset($query_array["views"]);
@@ -113,30 +156,16 @@ if(1==1){
         $this_page_count = mysql_num_rows($result);
 
         //Add new page, if there is not enought space to display the next category
-        $needed_height=count($viewdef_array["fields"]) * $this_page_count * ($col_height_1/10) + $pdf->GetY();
+        $needed_height=count($viewdef_array["fields"]) * $this_page_count * ($draw["col_height_1"]/10) + $pdf->GetY();
         if($needed_height>260){
-
-            //Print Footer
-            $pdf->SetFont('Arial','B',$font_size_footer);
-            $pdf->SetY(-15);
-            $pdf->Cell(50,7,__("Printed").": ".date("Y-m-d, H:i"),0,0,'L');
-            $pdf->SetX(100);
-            $pdf->Cell(50,7,$pdf->PageNo(),0,1,'L');
-
-            //Start new Page
-            $pdf->AddPage();
-            $pdf->Ln();
+            $pdf=pdf_draw_footer($pdf, $draw);
+            $pdf=pdf_draw_new_page($pdf, $draw);
+            $pdf=pdf_draw_headline_1($pdf, $draw, "");
         }
 
-        //Image
-        if(isset($viewdef_array["image"]) AND $viewdef_array["image"]!=""){
-            //$pdf->Image($viewdef_array["image"], $pdf->getx(), $pdf->gety() );
-        }
         //Headline of each category
         if(isset($viewdef_array["headline"]) AND $viewdef_array["headline"]!=""){
-            $pdf->Ln();
-            $pdf->SetFont('Arial','B',$font_size_headline_2);
-            $pdf->Cell(50,7,$viewdef_array["headline"],0,1,'L');
+            $pdf=pdf_draw_headline_2($pdf, $draw, $viewdef_array["headline"]);
         }
 
         //Body
@@ -149,29 +178,18 @@ if(1==1){
                     if(!isset($field["show"]) OR $field["show"]!="n"){
 
                         $show_value = special_field_converting($myrow, $field, $db, "system");
-                        $pdf->Cell($col_width_1,$col_height_1,$field["head"],'B',0,'L');
-                        $pdf->Cell($col_width_2,$col_height_1,$show_value,'B',1,'L');
-
+                        $pdf=pdf_draw_row($pdf, $draw, $field["head"].":", $show_value);
                     }
                 }
+                $pdf=pdf_draw_ln($pdf);
             }while ($myrow = mysql_fetch_array($result));
         }else{
-            $pdf->Cell($col_width_1,$col_height_1,__("No Results"),'B',0,'L');
-            $pdf->Cell($col_width_2,$col_height_1,"",'B',1,'L');
+            $pdf=pdf_draw_row($pdf, $draw, __("No Results"), "");
         }
     }
 }
 
-
 //Print Footer of last Page
-$pdf->SetFont('Arial','B',$font_size_footer);
-$pdf->SetY(-15);
-$pdf->Cell(50,7,__("Printed").": ".date("Y-m-d, H:i"),0,0,'L');
-$pdf->SetX(100);
-$pdf->Cell(50,7,$pdf->PageNo(),0,1,'L');
-
-//Send PDF
-$pdf->Output();
-$pdf->Close();
-
+$pdf=pdf_draw_footer($pdf, $draw);
+$pdf=pdf_end($pdf);
 ?>
