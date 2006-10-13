d26215fc06b1aca8f961d22412c9fdcb6816ebb8
dldave <dldave@0fda6dae-eacc-4bf0-a90c-2bab3023e2ff>
2006-10-13 13:57:02 +0000

    Close <br> tag
    git-svn-id: svn://svn.code.sf.net/p/open-audit/code/trunk@564 0fda6dae-eacc-4bf0-a90c-2bab3023e2ff

--- system.php
+++ system.php
@@ -379,7 +379,7 @@ while (list ($viewname, $viewdef_array) = @each ($query_array["views"])) {
     if(isset($_REQUEST["monitor"])){
         echo "<input type=\"hidden\" name=\"monitor\" value=\"".$_REQUEST["monitor"]."\" />\n";
     }
-    echo "<br><a href=\"#\" onClick=\"document.form_export.submit();\">".__("Export this Page to PDF")."</a>\n";
+    echo "<br /><a href=\"#\" onClick=\"document.form_export.submit();\">".__("Export this Page to PDF")."</a>\n";
     echo "</form>\n";
 
 
