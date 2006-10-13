ef70391fff5507385a5d914a29cc00fa15333c11
dldave <dldave@0fda6dae-eacc-4bf0-a90c-2bab3023e2ff>
2006-10-13 17:01:37 +0000

    Show full OS name in statistics to prevent confusing sorts and searches
    git-svn-id: svn://svn.code.sf.net/p/open-audit/code/trunk@569 0fda6dae-eacc-4bf0-a90c-2bab3023e2ff

--- list_viewdef_statistic_os.php
+++ list_viewdef_statistic_os.php
@@ -2,15 +2,15 @@
 
 $query_array=array("headline"=>__("Statistic for Operating Systems"),
                    "sql"=>"SELECT
-                               system_os_name,
+                               system_os_name as full_system_os_name,
                                COUNT( system_uuid ) AS count_item,
                                round( 100 / (SELECT count(system_uuid) FROM system  WHERE system_os_name != '') * COUNT( system_uuid ), $round_to_decimal_places ) AS percentage
                            FROM system
                            WHERE (1)
-                           GROUP BY system_os_name",
+                           GROUP BY full_system_os_name",
                    "sort"=>"count_item",
                    "dir"=>"DESC",
-                   "fields"=>array("10"=>array("name"=>"system_os_name",
+                   "fields"=>array("10"=>array("name"=>"full_system_os_name",
                                                "head"=>__("OS"),
                                                "show"=>"y",
                                                "link"=>"n",
