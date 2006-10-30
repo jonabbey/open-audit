237258758154b892e7e2f975537f20dfeca7c791
andyhull <andyhull@0fda6dae-eacc-4bf0-a90c-2bab3023e2ff>
2006-10-30 16:33:37 +0000

    Added fqdn to launcher, substitutes .local on any domain which does not resolve, so server.mydomain will resolve to server.mydonain.local for backward compatible launch with older NT4 style domainitems.
    git-svn-id: svn://svn.code.sf.net/p/open-audit/code/trunk@599 0fda6dae-eacc-4bf0-a90c-2bab3023e2ff

--- launch.php
+++ launch.php
@@ -2,13 +2,21 @@
 
 if(isset($_GET["application"])) $application=$_GET["application"];
 if(isset($_GET["hostname"])) $hostname=$_GET["hostname"];
+if(isset($_GET["domain"])) $domain=$_GET["domain"];
 if(isset($_GET["ext"])) $ext=$_GET["ext"];
+
+
+if (!ereg('/.',$domain)){
+    $domain = $domain.".local";
+    }
+
+$fqdn=$hostname.".".$domain;
 
 SWITCH($application){
     case "http":
     case "https":
     case "ftp":
-        header("Location: ".$application."://".$hostname);
+        header("Location: ".$application."://".$fqdn);
     break;
     default:
         //Reading the template
@@ -16,7 +24,8 @@ SWITCH($application){
         $buffer=file("launch_filedef_".$application.".txt");
         $buffer=implode("",$buffer);
         //Replacing Hostname
-        $buffer=str_replace ( "NAME", $hostname, $buffer );
+        $fqdn = $hostname.
+        $buffer=str_replace ( "NAME", $fqdn, $buffer );
 
         //Send to Browser
         header("Content-type: application/force-download");
