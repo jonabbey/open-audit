3ffe054cabcbc99aabb092467b3bfc148aa27350
andyhull <andyhull@0fda6dae-eacc-4bf0-a90c-2bab3023e2ff>
2007-05-24 11:20:15 +0000

    Added Comments header to index.php to include ref to license
    git-svn-id: svn://svn.code.sf.net/p/open-audit/code/trunk@726 0fda6dae-eacc-4bf0-a90c-2bab3023e2ff

--- index.php
+++ index.php
@@ -1,4 +1,20 @@
 <?php
+
+/**
+* @version $Id: index.php  24th May 2007
+* @author The Open Audit Developer Team
+* @objective Index Page for Open Audit.
+* @package open-audit (www.open-audit.org)
+* @copyright Copyright (C) open-audit.org All rights reserved.
+* @license http://www.gnu.org/copyleft/gpl.html GNU/GPL, see ../gpl.txt
+* Open-Audit is free software. This version may have been modified pursuant
+* to the GNU General Public License, and as distributed it includes or
+* is derivative of works licensed under the GNU General Public License or
+* other free or open source software licenses.
+* See www.open-audit.org for further copyright notices and details.
+*
+*/ 
+
 $page = "";
 $extra = "";
 $software = "";
