5d34ca4480b72844f418b487ef8c7ab7b64a591a
mikeyrb <mikeyrb@0fda6dae-eacc-4bf0-a90c-2bab3023e2ff>
2007-09-28 21:04:09 +0000

    Correct latest version in index.php
    git-svn-id: svn://svn.code.sf.net/p/open-audit/code/trunk@826 0fda6dae-eacc-4bf0-a90c-2bab3023e2ff

--- index.php
+++ index.php
@@ -21,7 +21,7 @@ $extra = "";
 $software = "";
 $count = 0;
 $total_rows = 0;
-$latest_version = "07.08.01";
+$latest_version = "07.08.28";
 
 // Check for config, otherwise run setup
 @(include_once "include_config.php") OR die(header("Location: setup.php"));
