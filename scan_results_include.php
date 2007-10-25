55727cf6f971fd12bdd2a12468328a7fd2b4075b
munwin <munwin@0fda6dae-eacc-4bf0-a90c-2bab3023e2ff>
2007-10-25 07:32:11 +0000

    scanning pages committed
    git-svn-id: svn://svn.code.sf.net/p/open-audit/code/trunk@857 0fda6dae-eacc-4bf0-a90c-2bab3023e2ff

---
+++ scan_results_include.php
@@ -0,0 +1,23 @@
+<?php
+$page = "";
+$extra = "";
+$software = "";
+$count = -1;
+if (isset($_GET['software'])) {$software = $_GET['software'];} else {}
+if (isset($_GET['sort'])) {$sort = $_GET['sort'];} else {$sort= "system_name";}
+include "include.php";
+
+echo "<td valign=\"top\">\n";
+echo "<div class=\"main_each\">";
+echo "<table border=\"0\" cellpadding=\"0\" cellspacing=\"0\">\n";
+echo "<tr>\n";
+echo "  <td class=\"contenthead\">Network Monitoring.<br />&nbsp;</td>\n";
+echo "</tr>\n";
+echo "<tr>\n";
+echo "<td width=\"20%\"><b>Host</b></td>\n";
+echo "<td align=\"center\" width=\"20%\"><b>Type</b></td>\n";
+echo "<td align=\"center\" width=\"20%\"><b>Detail</b></td>\n";
+echo "<td align=\"center\" width=\"20%\"><b>Time</b></td>\n";
+echo "<td align=\"center\" width=\"20%\"><b>Result</b></td>\n";
+echo "</tr>\n";
+
