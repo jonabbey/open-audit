8a44d3482f7d0af7314b16e11a804ceecc8df7dc
andyhull <andyhull@0fda6dae-eacc-4bf0-a90c-2bab3023e2ff>
2007-12-18 10:10:31 +0000

    Updated list_export_config.php
    git-svn-id: svn://svn.code.sf.net/p/open-audit/code/trunk@928 0fda6dae-eacc-4bf0-a90c-2bab3023e2ff

--- list_export_config.php
+++ list_export_config.php
@@ -1,20 +1,32 @@
 <?php
-
 include_once("include_config.php");
 include_once("include_functions.php");
 include_once("include_lang.php");
-
-
-
-//$config_newline=chr(13).chr(10);
-// 
+//
+//
+/*
+*
+* @version $Id: index.php  24th May 2007
+*
+* @author The Open Audit Developer Team
+* @objective Export Config Page for Open Audit.
+* @package open-audit (www.open-audit.org)
+* @copyright Copyright (C) open-audit.org All rights reserved.
+* @license http://www.gnu.org/copyleft/gpl.html GNU/GPL, see ../gpl.txt
+* Open-Audit is free software. This version may have been modified pursuant
+* to the GNU General Public License, and as distributed it includes or
+* is derivative of works licensed under the GNU General Public License or
+* other free or open source software licenses.
+* See www.open-audit.org for further copyright notices and details.
+*
+*/ 
 $config_newline="\r\n";
-
-
-
+//
+//
+//
 $this_config='audit_location = "r"'.$config_newline;
 $this_config=$this_config.'verbose = "y"'.$config_newline; 
-$this_config=$this_config.'audit_host="http://support.cmarine.local"'.$config_newline;
+$this_config=$this_config.'audit_host="http://openaudit.mydomain.local"'.$config_newline;
 $this_config=$this_config.'online = "yesxml"'.$config_newline; 
 $this_config=$this_config.'strComputer = ""'.$config_newline; 
 $this_config=$this_config.'ie_visible = "n" '.$config_newline;
@@ -23,11 +35,19 @@ $this_config=$this_config.'ie_submit_verbose = "n"'.$config_newline;
 $this_config=$this_config.'ie_form_page = audit_host + "/openaudit/admin_pc_add_1.php"'.$config_newline; 
 $this_config=$this_config.'non_ie_page = audit_host + "/openaudit/admin_pc_add_2.php"'.$config_newline; 
 $this_config=$this_config.'input_file = ""'.$config_newline; 
-$this_config=$this_config.'email_to = ""'.$config_newline; 
-$this_config=$this_config.'email_from = ""'.$config_newline; 
-$this_config=$this_config.'email_server = ""'.$config_newline; 
+$this_config=$this_config.'email_to = "openaudit@mydonain.com"'.$config_newline;    
+$this_config=$this_config.'email_from = "openaudit@mydonain.com"'.$config_newline;
+$this_config=$this_config.'email_sender = "Open Audit"'.$config_newline;
+$this_config=$this_config.'email_server = "mail.mydomain.com"'.$config_newline;  
+$this_config=$this_config.'email_port = "25"'.$config_newline;                
+$this_config=$this_config.'email_auth = "1"'.$config_newline;
+$this_config=$this_config.'email_user_id = "openaudit@mydonain.com"'.$config_newline;
+$this_config=$this_config.'email_user_pwd = "MailPassword"'.$config_newline;
+$this_config=$this_config.'email_use_ssl = "false"'.$config_newline;
+$this_config=$this_config.'email_timeout = "60"'.$config_newline;
 $this_config=$this_config.'audit_local_domain = "y"'.$config_newline;
-$this_config=$this_config.'local_domain = "LDAP://cmarine.local"'.$config_newline; 
+$this_config=$this_config.'domain_type = "ldap"'.$config_newline;
+$this_config=$this_config.'local_domain = "LDAP://mydomain.local"'.$config_newline; 
 $this_config=$this_config.'hfnet = "n"'.$config_newline; 
 $this_config=$this_config.'Count = 0'.$config_newline; 
 $this_config=$this_config.'number_of_audits = 10'.$config_newline; 
