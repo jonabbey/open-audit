ea81209850cf7bf6a691832db49a8d15a722f6b0
ef2007 <ef2007@0fda6dae-eacc-4bf0-a90c-2bab3023e2ff>
2009-05-22 10:47:13 +0000

    Same fix for Network printers
    git-svn-id: svn://svn.code.sf.net/p/open-audit/code/trunk@1170 0fda6dae-eacc-4bf0-a90c-2bab3023e2ff

--- admin_pc_add_2.php
+++ admin_pc_add_2.php
@@ -1393,7 +1393,9 @@ function insert_printer ($split){
       } else {
         // Update
        $sql  = "UPDATE other SET other_timestamp = '$timestamp', other_p_port_name = '$printer_network_name', ";
-       $sql .= "other_location = '$printer_location', other_description = '$printer_caption' WHERE other_ip_address = '" . ip_trans_to($printer_ip) . "'";
+       $sql .= "       other_location = '$printer_location', other_description = '$printer_caption', ";
+       $sql .= "       other_p_shared = '$printer_shared', other_p_share_name = '$printer_share_name' ";
+       $sql .= "WHERE other_ip_address = '" . ip_trans_to($printer_ip) . "'";
        if ($verbose == "y"){echo $sql . "<br />\n\n";}
        $result = mysql_query($sql) or die ('Insert Failed: ' . mysql_error() . '<br />' . $sql);
       }
